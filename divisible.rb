29ad962e3e994451c7d0fc3a9c5d9b984241ff15
KJ <kjloving1400@gmail.com>
2022-11-03 23:23:32 -0700

    added program files and instructions

---
+++ divisible.rb
@@ -0,0 +1,1 @@
+#Write a method that returns in an array all the numbers between 1 and 100 that are divisible by 2 or 3 or 5. Then call the method in your program and print out what it returns.  Call the program divisible.rb.
