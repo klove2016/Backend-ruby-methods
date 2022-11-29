0d574a3cff674d3c9cfa27eb9b3b4fe05b2ab22d
KJ <kjloving1400@gmail.com>
2022-11-29 13:50:40 -0800

    docalc fixed

--- block_function.rb
+++ block_function.rb
@@ -1,13 +1,7 @@
 
 
-def do_calc(a, b)
+def do_calc
     yield(7, 9)
 end
-    puts do_calc(7, 9) { |a, b| a + b }
-    puts do_calc(7, 9) { |a, b| a * b }
-  
-def do_calc_2
-    yield
-end
-    do_calc_2 {puts 7 + 9}
-    do_calc_2 {puts 7 * 9}
+puts do_calc {|a, b| a + b}
+puts do_calc {|a, b| a * b}
